977e3f9022765999f2e767ca0d1ac30666a9ba05
krasn <krasnoschek003@gmail.com>
2023-01-03 22:00:51 +1000

    add ViewModelFactory for create apps ViewModels

---
+++ app/src/main/java/com/example/foundation/views/ViewModelFactory.kt
@@ -0,0 +1,39 @@
+package com.example.foundation.views
+
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+import java.lang.reflect.Constructor
+
+class ViewModelFactory(
+    private val dependencies: List<Any>,
+    owner: SavedStateRegistryOwner
+) : AbstractSavedStateViewModelFactory(owner, null) {
+
+    override fun <T : ViewModel?> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T {
+        val constructors = modelClass.constructors
+        val constructor = constructors.maxByOrNull { it.typeParameters.size }!!
+
+        val dependenciesWithSavedState =dependencies + handle
+
+        val arguments = findDependencies(constructor, dependenciesWithSavedState)
+
+        @Suppress("UNCHECKED_CAST")
+        return constructor.newInstance(*arguments.toTypedArray()) as T
+    }
+
+    private fun findDependencies(constructor: Constructor<*>, dependencies: List<Any>): List<Any> {
+        val args = mutableListOf<Any>()
+
+        constructor.parameterTypes.forEach { parameterClass ->
+            val dependency = dependencies.first { parameterClass.isAssignableFrom(it.javaClass) }
+            args.add(dependency)
+        }
+        return args
+    }
+}
