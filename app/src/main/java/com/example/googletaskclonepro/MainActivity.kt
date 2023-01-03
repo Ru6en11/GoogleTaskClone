ad88170d422f4af19d60f05f49c200c0744d0a43
krasn <krasnoschek003@gmail.com>
2023-01-03 20:41:39 +1000

    connected view binding

--- app/src/main/java/com/example/googletaskclonepro/MainActivity.kt
+++ app/src/main/java/com/example/googletaskclonepro/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.googletaskclonepro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.googletaskclonepro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
     }
 }
