a6f2f6b45a01b84aef6ec23bee01d06b22a26364
krasn <krasnoschek003@gmail.com>
2023-01-18 00:04:08 +1000

    create bottom dialog for create task

--- app/src/main/java/com/example/googletaskclonepro/views/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/googletaskclonepro/views/tasks/TasksFragment.kt
@@ -1,15 +1,21 @@
 package com.example.googletaskclonepro.views.tasks
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import com.example.foundation.views.BaseFragment
 import com.example.foundation.views.BaseScreen
 import com.example.foundation.views.screenViewModel
+import com.example.googletaskclonepro.R
+import com.example.googletaskclonepro.databinding.CreateTaskBottomSheetDialogBinding
 import com.example.googletaskclonepro.databinding.FragmentTasksBinding
 import com.example.googletaskclonepro.model.task.Task
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class TasksFragment : BaseFragment() {
 
@@ -17,6 +23,7 @@ class TasksFragment : BaseFragment() {
 
     private lateinit var binding: FragmentTasksBinding
     override val viewModel: TasksViewModel by screenViewModel()
+    private lateinit var createTaskDialog: BottomSheetDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,8 +32,13 @@ class TasksFragment : BaseFragment() {
     ): View {
         binding = FragmentTasksBinding.inflate(inflater, container, false)
 
+        createTaskDialog = BottomSheetDialog(requireContext(), R.style.DialogStyle)
+        createTaskDialog.window?.addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
+
+        renderCreateTaskDialog()
+
         binding.createItemFab.setOnClickListener {
-            viewModel.createTask(Task(isCompleted = true, text = "", additionalInfo = "", isFavourite = true))
+            createTaskDialog.show()
         }
 
         return binding.root
@@ -38,6 +50,53 @@ class TasksFragment : BaseFragment() {
         viewModel.tasks.observe(viewLifecycleOwner) {
             Log.d("tag", "${it}")
         }
+    }
+
+    private fun renderCreateTaskDialog() {
+
+        val dialogBinding = CreateTaskBottomSheetDialogBinding.inflate(LayoutInflater.from(requireContext()), null, false)
+        createTaskDialog.setContentView(dialogBinding.root)
+
+        dialogBinding.saveTaskButton.isEnabled = false
+
+        dialogBinding.additionalInfoImageBtn.setOnClickListener {
+            dialogBinding.additionalInfoEditText.visibility = View.VISIBLE
+        }
+        dialogBinding.addInFavouriteCheckBox.setOnCheckedChangeListener { _, isChecked ->
+            dialogBinding.addInFavouriteCheckBox.setButtonDrawable(
+                if (isChecked) R.drawable.ic_star else R.drawable.ic_star_border
+            )
+        }
+
+        dialogBinding.taskTitleEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                dialogBinding.saveTaskButton.isEnabled = s?.isNotBlank() == true
+            }
+        })
+
+        dialogBinding.saveTaskButton.setOnClickListener {
+            val task = Task(
+                isCompleted = false,
+                text = dialogBinding.taskTitleEditText.text.toString(),
+                additionalInfo = dialogBinding.additionalInfoEditText.text.toString(),
+                isFavourite = dialogBinding.addInFavouriteCheckBox.isChecked
+            )
+            viewModel.createTask(task)
+            createTaskDialog.dismiss()
+        }
+        dialogBinding.taskTitleEditText.requestFocus()
+
+        createTaskDialog.setOnDismissListener {
+            dialogBinding.apply {
+                taskTitleEditText.text.clear()
+                additionalInfoEditText.text.clear()
+                additionalInfoEditText.visibility = View.GONE
+                addInFavouriteCheckBox.isChecked = false
+            }
+        }
+
     }
 
 }
